3e91b66206960ae87ada4d275824c286b60ed877
carrera.android <carrera.android@educacionit.com>
2020-12-21 03:28:55 -0300

    Create ViewExtensions with hide and show view methods

---
+++ app/src/main/java/com/example/theluckyapptest/extensions/ViewExtensions.kt
@@ -0,0 +1,17 @@
+package com.example.theluckyapptest.extensions
+
+import android.view.View
+
+fun View.show(): View {
+    if (visibility != View.VISIBLE) {
+        visibility = View.VISIBLE
+    }
+    return this
+}
+
+fun View.hide(): View {
+    if (visibility != View.GONE) {
+        visibility = View.GONE
+    }
+    return this
+}
